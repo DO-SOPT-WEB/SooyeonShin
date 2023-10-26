68c64c2cbb611ca3d041ada5f2be4684d72794b3
신수연 <sooy@shinsooyeon-MacBookPro.local>
2023-10-27 06:12:53 +0900

    feat : 리스트 삭제 구현

--- week2/assign4/index.js
+++ week2/assign4/index.js
@@ -1,90 +1,113 @@
 const INIT_BALANCE =0;
-const HISTORY_LIST=[
-  {type:"income",category:"과외비",content:"10월 월급",amount:50000},
-  {type:"expense",category:"식비",content:"마라탕",amount:10000},
-  {type:"income",category:"과외비",content:"10월 월급",amount:50000},
-  {type:"expense",category:"식비",content:"커피",amount:6000}];
+let HISTORY_LIST=[
+  {id:0,type:"income",category:"과외비",content:"10월 월급",amount:50000},
+  {id:1,type:"expense",category:"식비",content:"마라탕",amount:10000},
+  {id:2,type:"income",category:"과외비",content:"10월 월급",amount:50000},
+  {id:3,type:"expense",category:"식비",content:"커피",amount:6000}];
 
   //list 추가를 위한 변수들
-let listcontainer;
+let itemContainer;
 let category;
 let content;
 let amount;
-let button;
+// let button;
 
 let totalIncome=0;
 let totalExpense=0;
 
-const listcontent = document.getElementById('lists');
+const listcontent = document.getElementById('lists'); //내역리스트 담는 ul태그
+const BALANCE = document.getElementById('balance');
+let balance=INIT_BALANCE;
+BALANCE.textContent= balance;
 
 //각 list의 정보 보여주는 함수 (중복해서 쓰여서 따로 뺌)
-const showList =(list,idx)=>{
-  listcontainer = document.createElement('li');
+const showList =(list)=>{
+  itemContainer = document.createElement('li');
       
   category = document.createElement('span');
   category.textContent=list.category;
-  listcontainer.appendChild(category);
+  itemContainer.appendChild(category);
 
   content = document.createElement('span');
   content.textContent=list.content;
-  listcontainer.appendChild(content);
+  itemContainer.appendChild(content);
 
   amount = document.createElement('span');
   amount.textContent=list.amount;
-  listcontainer.appendChild(amount);
+  itemContainer.appendChild(amount);
   amount.classList.add(list.type);
 
-  let button = document.createElement('button');
+  const button = document.createElement('button');
   button.textContent = "x";
   button.classList.add("xbtn");
-  listcontainer.appendChild(button);
-  button.addEventListener('click',()=>{console.log(idx,"삭제 버튼")});
+  itemContainer.appendChild(button);
+  //x버튼 클릭시 이벤트
+  button.addEventListener('click',()=>{
+    console.log(list.id,"삭제 버튼"); 
+    deleteItem(list.id);
+    console.log(HISTORY_LIST)
+  });
 
-  listcontent.appendChild(listcontainer);
+  listcontent.appendChild(itemContainer);
 
-  //총수익, 총지출 구하기
-  if(list.type==="income"){
-    totalIncome += list.amount;
-  } else if(list.type === "expense"){
-    totalExpense +=list.amount;
-  }
+  
 }
 
-  window.onload = ()=>{ //최초 실행시 렌더링
+const calculateTotal=()=>{
+  totalExpense=0;
+  totalIncome=0;
+  HISTORY_LIST.forEach((item)=>{
+    //총수익, 총지출 구하기
+    if(item.type==="income"){
+      totalIncome += item.amount;
+    } else if(item.type === "expense"){
+      totalExpense +=item.amount;
+    }
+  })
+  //총수익, 총지출 html에 표시
+  document.getElementById('totalIncome').textContent = totalIncome;
+  document.getElementById('totalExpense').textContent = totalExpense;
 
-    HISTORY_LIST.forEach((list,idx) =>showList(list,idx));
+  balance=totalIncome-totalExpense;
+  BALANCE.textContent=balance;
+}
 
-    //총수익, 총지출 html에 표시
-    document.getElementById('totalIncome').textContent = totalIncome;
-    document.getElementById('totalExpense').textContent = totalExpense;
+window.onload = ()=>{ //최초 실행시 렌더링
 
-  }
+  HISTORY_LIST.forEach((list) =>showList(list));
+  calculateTotal();
+
+  
+
+}
 
   //수입/지출 필터링 ->filter
   
-  document.getElementById('incomeCheckbox').addEventListener('change',()=>filterList());
-  document.getElementById('expenseCheckbox').addEventListener('change',()=>filterList());
+document.getElementById('incomeCheckbox').addEventListener('change',()=>filterList());
+document.getElementById('expenseCheckbox').addEventListener('change',()=>filterList());
 
-  const filterList=()=>{
-    const showIncome = document.getElementById('incomeCheckbox').checked;
-    const showExpense = document.getElementById('expenseCheckbox').checked;
+const filterList=()=>{
+  const showIncome = document.getElementById('incomeCheckbox').checked;
+  const showExpense = document.getElementById('expenseCheckbox').checked;
 
-    console.log(showIncome,showExpense);
+  console.log(showIncome,showExpense);
 
-    listcontent.innerHTML='';//화면에 보여줄 리스트 초기화 (안해주면 중복해서 뜸)
+  listcontent.innerHTML='';//화면에 보여줄 리스트 초기화 (안해주면 중복해서 뜸)
 
-    const new_list=HISTORY_LIST.filter(list =>{
-      if(list.type==="income" && showIncome) return true;
-      if(list.type==="expense" && showExpense) return true;
-      return false;
-    });
+  HISTORY_LIST.filter(list =>{
+    if(list.type==="income" && showIncome) return true;
+    if(list.type==="expense" && showExpense) return true;
+    return false;
+  }).forEach((list) =>showList(list));
 
-    new_list.forEach((list,idx) =>showList(list,idx));
-  
-  }
+};
 
-  //수입/지출 필터링 end
-
-  //리스트 삭제 start
+//리스트 삭제
+const deleteItem=(deleteid)=>{
+  HISTORY_LIST = HISTORY_LIST.filter(item => item.id!==deleteid);
+  listcontent.innerHTML='';
+  HISTORY_LIST.forEach(list=>showList(list));
+  calculateTotal();
+}
 
 
