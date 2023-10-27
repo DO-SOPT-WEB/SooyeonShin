4b60dab97419f74d1f7faeef4a111044a470acdf
신수연 <sooy@shinsooyeon-MacBookPro.local>
2023-10-27 23:08:39 +0900

    feat: let으로 수정

--- week2/assign1/index.js
+++ week2/assign1/index.js
@@ -1,13 +1,13 @@
-var parisContainers = document.querySelectorAll('.parisContainer');
+let parisContainers = document.querySelectorAll('.parisContainer');
 
 parisContainers.forEach(function(container) {
     container.addEventListener('mouseover', function() {
-        var content = container.querySelector('div');
+        let content = container.querySelector('div');
         content.classList.add('show-paris-content');
     });
 
     container.addEventListener('mouseout', function() {
-        var content = container.querySelector('div');
+        let content = container.querySelector('div');
         content.classList.remove('show-paris-content')
         //content.parentNode.removeChild(content);
     });
@@ -15,13 +15,13 @@ parisContainers.forEach(function(container) {
 
 // 버튼 투명도
 window.addEventListener('scroll', ()=> {
-  var button = document.getElementById('top');
+  let button = document.getElementById('top');
   
   // 최대 스크롤 가능한 높이
-  var maxScrollHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  let maxScrollHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
   
   // 현재 스크롤된 높이의 비율 계산 (0에서 1 사이의 값)
-  var scrollPercentage = window.scrollY / maxScrollHeight;
+  let scrollPercentage = window.scrollY / maxScrollHeight;
   
   // 버튼의 투명도를 스크롤 비율에 따라 설정
   button.style.opacity = scrollPercentage;
